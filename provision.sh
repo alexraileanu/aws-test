fcb8c0c62f95d8456e915e78c8f7cba7f9836eb2
Alex <alexraileanu@me.com>
2019-02-13 17:00:15 +0100

    updated provision script; added uwsgi to requirements

--- provision.sh
+++ provision.sh
@@ -4,7 +4,10 @@ sudo yum install -y git python3
 git clone https://github.com/alexraileanu/aws-test $HOME/aws-test
 cd $HOME/aws-test
 
+sudo pip3.7 install virtualenv
+
 virtualenv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
 sudo cp resources/api.service /lib/systemd/system/api.service
