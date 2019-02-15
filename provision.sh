69b6ea169e6ad22d58290e485d9521697b4f2fc9
Alex <alexraileanu@me.com>
2019-02-15 11:14:50 +0100

    added nginx config

--- provision.sh
+++ provision.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo yum install -y git python3 python3-devel gcc
+sudo amazon-linux-extras install nginx1.12
+
 git clone https://github.com/alexraileanu/aws-test $HOME/aws-test
 cd $HOME/aws-test
 
@@ -9,6 +11,8 @@ sudo pip3.7 install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+sudo cp resources/api.conf /etc/nginx/conf.d/api
 
 sudo cp resources/api.service /lib/systemd/system/api.service
 sudo systemctl daemon-reload
