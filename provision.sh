6a9ad1bd70da6f33ca03596030f6a4e0b71c361d
Alex <alexraileanu@me.com>
2019-02-15 11:20:21 +0100

    proper nginx conf name; restart nginx after copy

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo yum install -y git python3 python3-devel gcc
-sudo amazon-linux-extras install nginx1.12
+sudo amazon-linux-extras -y install nginx1.12
 
 git clone https://github.com/alexraileanu/aws-test $HOME/aws-test
 cd $HOME/aws-test
@@ -12,7 +12,8 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-sudo cp resources/api.conf /etc/nginx/conf.d/api
+sudo cp resources/api.conf /etc/nginx/conf.d/api.conf
+sudo systemctl restart nginx
 
 sudo cp resources/api.service /lib/systemd/system/api.service
 sudo systemctl daemon-reload
